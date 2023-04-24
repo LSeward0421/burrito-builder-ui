a214c7a273097924dbd4fa8bbcb72b26ba1cfe8f
Lauren Seward <lauren.unal@gmail.com>
2023-04-24 10:20:55 -0500

    Bug: update app to hold a state of orders and fix api call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
-    super();
+    super(props);
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
     getOrders()
+    .then(data => {
+      this.setState({orders: data.orders})
+    })
       .catch(err => console.error('Error fetching:', err));
   }
 
@@ -21,7 +27,6 @@ class App extends Component {
           <h1>Burrito Builder</h1>
           <OrderForm />
         </header>
-
         <Orders orders={this.state.orders}/>
       </main>
     );
