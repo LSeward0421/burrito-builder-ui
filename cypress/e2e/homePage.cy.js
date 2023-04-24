4f0f808c00b6fe48c4bfeb1de671e5b7f43b998c
Lauren Seward <lauren.unal@gmail.com>
2023-04-24 12:34:06 -0500

    Test: add test for successful new order submission

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -6,7 +6,9 @@ describe('Home Page', () => {
     cy.visit('http://localhost:3000/')
     cy.wait('@getOrders')
   });
+
   // As a user, when I navigate to the website I should see the name of the website, form to put my name, ingredient buttons, a submit button and any previous orders. 
+
   it('should see the order input field, ingredient buttons, previous orders and a submit button', () => {
     cy.get('h1').should('contain.text', 'Burrito Builder')
     cy.get('input[name=name]').should('have.attr', 'placeholder', 'Name')
@@ -20,11 +22,36 @@ describe('Home Page', () => {
     cy.get(':nth-child(1) > h3').should('contain', 'Larry');
     cy.get(':nth-child(1) > .ingredient-list > :nth-child(1)').should('contain', 'beans');
     cy.get(':nth-child(1) > .ingredient-list > :nth-child(2)').should('contain', 'rice');
-    cy.get(':nth-child(1) > .ingredient-list > :nth-child(3)').should('contain', 'cheese');
+    cy.get(':nth-child(1) > .ingredient-list > :nth-child(3)').should('contain', 'queso fresco');
   
     cy.get(':nth-child(2) > h3').should('contain', 'Tayyar');
     cy.get(':nth-child(2) > .ingredient-list > :nth-child(1)').should('contain', 'steak');
     cy.get(':nth-child(2) > .ingredient-list > :nth-child(2)').should('contain', 'lettuce');
     cy.get(':nth-child(2) > .ingredient-list > :nth-child(3)').should('contain', 'pico de gallo');
   })
+
+  // As a user, when I add my name, select ingredients and click submit order, I should see my new order on the screen with the correct information. 
+
+  it('should be able to submit a new order', () => {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', { fixture: 'newOrder' }).as('postOrder');
+
+    cy.get('.order').should('have.length', 2);
+
+    cy.get('input[name=name]').type('Maggie');
+    cy.get('[name="steak"]').click();
+    cy.get('[name="beans"]').click();
+    cy.get('[name="queso fresco"]').click();
+    cy.get('form > p').contains('Order: steak, beans, queso fresco');
+    cy.get('button').contains('Submit Order').click();
+    cy.wait('@postOrder');
+
+    cy.get('input[name=name]').should('have.value', '');
+    cy.get('form > p').contains('Order: Nothing selected');
+    cy.get('.order').should('have.length', 3);
+    cy.get(':nth-child(3) > h3').should('contain', 'Maggie');
+    cy.get(':nth-child(3) > .ingredient-list > :nth-child(1)').should('contain', 'steak');
+    cy.get(':nth-child(3) > .ingredient-list > :nth-child(2)').should('contain', 'beans');
+    cy.get(':nth-child(3) > .ingredient-list > :nth-child(3)').should('contain', 'queso fresco');
+  });
+  
 })
